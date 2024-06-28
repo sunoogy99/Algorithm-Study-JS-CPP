6def744c0c276e4067c09a8a4c86dab92e9028bb
sunoogy99 <sunoogy99@naver.com>
2024-06-29 00:37:19 +0900

    [코드트리] [BFS] 네 방향 탈출 가능 여부 판별하기 ✅ (24.06.27)

--- Codetree/BFS/네 방향 탈출 가능 여부 판별하기/solution.js
+++ Codetree/BFS/네 방향 탈출 가능 여부 판별하기/solution.js
@@ -1,3 +1,5 @@
+// 문제 : https://www.codetree.ai/missions/2/problems/determine-escapableness-with-4-ways/description
+
 /*
     뱀이 있는 경우 : 0
     뱀이 없는 경우 : 1
@@ -15,8 +17,60 @@ let graph = [];
 const dx = [-1, 0, 1, 0];
 const dy = [0, 1, 0, -1];
 
+class Queue {
+    constructor() {
+        this._arr = []; // 값을 저장할 객체
+    }
+
+    enqueue(item) {
+        this._arr.push(item);
+    }
+
+    dequeue() {
+        return this._arr.shift();
+    }
+
+    isEmpty() {
+        return this._arr.length === 0 ? true : false;
+    }
+}
+
 const solutionByBFS = (graph, visited) => {
+    const inRange = (x, y) => {
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
+
+    const canGo = (x, y) => {
+        if (!inRange(x, y)) return false;
+        else if (visited[x][y] || !graph[x][y]) return false;
+        else return true;
+    }
     
+    const queue = new Queue();
+
+    queue.enqueue([0, 0]);
+    visited[0][0] = true;
+
+    while (!queue.isEmpty()) {
+        let [curX, curY] = queue.dequeue();
+
+        for (let i = 0; i < 4; i++) {
+            let newX = curX + dx[i];
+            let newY = curY + dy[i];
+
+            if (canGo(newX, newY)) {
+                queue.enqueue([newX, newY]);
+                visited[newX][newY] = true;
+            }
+        }
+    }
+
+    if (visited[N - 1][M - 1]) {
+        console.log(1);
+    }
+    else {
+        console.log(0);
+    }
 }
 
 const solutionByDFS = (graph, visited) => {
