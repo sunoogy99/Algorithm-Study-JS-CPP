587498181827d67c123d343bb66d90d11f7057e2
kwonsoonook <sunoogy99@naver.com>
2024-07-12 16:31:21 +0900

    [SWEA] [D5] 달란트 ⚠️ (24.07.12)

---
+++ C++/SWEA/D5/달란트2/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+int T, N, P;
+int groupElement[101];
+
+unsigned long long findMax() {
+	for (int i = 1; i <= P; i++) {
+		groupElement[i] += N / P;
+	}
+
+	int remain = N % P;
+	for (int i = 1; i <= remain; i++) {
+		groupElement[i] += 1;
+	}
+
+	unsigned long long sum = 1;
+	for (int i = 1; i <= P; i++) {
+		sum *= groupElement[i];
+	}
+
+	return sum;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		cin >> N >> P;
+
+		memset(groupElement, 0, sizeof(groupElement));
+		unsigned long long maxVal = findMax();
+		cout << '#' << i << ' ' << maxVal << '\n';
+	}
+
+	return 0;
+}
